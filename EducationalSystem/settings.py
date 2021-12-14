5a94cae0e1545eb282533bd80355833bbf41d316
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2021-12-14 09:24:22 +0330

    set Pagination

--- EducationalSystem/settings.py
+++ EducationalSystem/settings.py
@@ -182,3 +182,8 @@ LOGGING = {
         'level': 'WARNING',
     },
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
